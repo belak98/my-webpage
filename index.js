c7a6bdc43f40b07927aaaf6e37d6a9026fe00128
Kaleb Van Heusen <84359465+belak98@users.noreply.github.com>
2021-06-10 02:28:06 -0700

    uploading new files
    Added files for html css and javascript. also added a gif source file required for the code to run properly.

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('button-click')
+
+button.onmouseover = function () {
+  this.style.backgroundColor = '#83B9F9'
+}
+
+button.onmouseout = function () {
+  this.style.backgroundColor = '#245DA1'
+}
