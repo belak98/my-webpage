1ca01ed8139dfa59a1fa5f75856943a21fc11fa3
Kaleb Van Heusen <84359465+belak98@users.noreply.github.com>
2021-06-23 22:17:18 -0700

    Added major changes
    Changed the page layout to resemble more of what the design goal is.
    
    Moving away from the "novice" appearance of the previous iteration.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const button = document.getElementById('button-click')
-
-button.onmouseover = function () {
-  this.style.backgroundColor = '#83B9F9'
-}
-
-button.onmouseout = function () {
-  this.style.backgroundColor = '#245DA1'
+function testAlert() {
+  alert("Hey  you found me!");
 }
